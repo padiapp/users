5a758620010fca298ce77a50ad26507172cba89b
pujie <pw.prayitno@gmail.com>
2017-10-23 16:47:17 +0700

    add setting for email

--- application/controllers/Mailer.php
+++ application/controllers/Mailer.php
@@ -3,19 +3,37 @@ class Mailer extends CI_Controller{
     function __construct(){
         parent::__construct();
     }
+    function index(){
+        echo 'this is index';
+    }
     function sendmail(){
         $this->load->library('email');
         //$this->config->load('padimail');
-        $config['protocol'] = 'sendmail';
-        $config['mailpath'] = '/usr/sbin/sendmail';
-        $config['charset'] = 'iso-8859-1';
-        $config['wordwrap'] = false;
+        //$config['protocol'] = 'sendmail';
+        //$config['mailpath'] = '/usr/sbin/sendmail';
+        //$config['charset'] = 'iso-8859-1';
+        //$config['wordwrap'] = false;
         
+
+
+        $config['protocol']    = 'smtp';
+        $config['smtp_host']    = 'ssl://smtp.gmail.com';
+        $config['smtp_port']    = '465';
+        $config['smtp_timeout'] = '7';
+        $config['smtp_user']    = 'pw.prayitno@gmail.com';
+        $config['smtp_pass']    = '******';
+        $config['charset']    = 'utf-8';
+        $config['newline']    = "\r\n";
+        $config['mailtype'] = 'text'; // or html
+        $config['validation'] = TRUE; // bool whether to validate email or not      
+
+
+
         $this->email->initialize($config);
         $this->email->from('puji@padi.net.id');
         $this->email->to('pw.prayitno@gmail.com');
         $this->email->subject('Test Email');
-        $this->email->message($this->welcomeusertemplate('Puji','pw.prayitno@gmail.com','test'));
+        print_r($this->email->message($this->welcomeusertemplate('Puji','pw.prayitno@gmail.com','test')));
     }
     function welcomeusertemplate($username,$email,$password){
         $txt = 'Halo ' . $username . ' ' . PHP_EOL;
